048b02e9da5cf200ed77b046023abacd76d2efb9
spawnmarvel <espenkleivane@gmail.com>
2021-09-06 20:24:53 +0200

    ms learn

--- 6_powershell/windows_features.ps1
+++ 6_powershell/windows_features.ps1
@@ -6,8 +6,9 @@ Get-WindowsFeature -Name "net*"
 # [X] .NET Framework 3.5 Features                         NET-Framework-Features         Installed
 # [...]
 Remove-WindowsFeature -Name "NET-Framework-Features" -Verbose
-
-# was needs .net 3.5
-Get-WindowsFeature -Name "*was*"
+# WAS needs .net 3.5
+Get-WindowsFeature -Name "*WAS*"
 Remove-WindowsFeature -Name "WAS-NET-Environment" -Verbose
+# Check .Net
+Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -Match '^(?!S)\p{L}'} | Select PSChildName, version
  
