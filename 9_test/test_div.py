a58592058ebde705c2188b745ec895690dc009cc
janespen <58297492+janespen@users.noreply.github.com>
2021-11-29 21:15:36 +0100

    labs

---
+++ 9_test/test_div.py
@@ -0,0 +1,14 @@
+dict = {}
+dict["key1"] = 123456
+dict["key2"] = 456789
+dict["key3"] = 789456
+for k, v in dict.items():
+    print(k, v)
+print(dict.get("key2"))
+rm_v = dict.pop("key3")
+print(dict)
+new_v = {"key4":99999}
+dict.update(new_v)
+print(dict)
+
+  
