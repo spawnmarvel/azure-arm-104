f56cf497b616cb6d230dd35d95ff361bb817b4ed
spawnmarvel <espenkleivane@gmail.com>
2022-01-15 15:48:39 +0100

    Test success 15.01.2022 2. Linux VM in rg xx, deployed to vnet/subnet in rg yy

--- 3_compute/vm_simple_lin/deploy_vm.ps1
+++ 3_compute/vm_simple_lin/deploy_vm.ps1
@@ -79,7 +79,7 @@ New-AzResourceGroupDeployment -Name $deployName `
   -deployToSubnet $subnetDeployTmp `
   -ResourceGroupName $resourceGrVM.ResourceGroupName `
   -virtualNetworkId $vnetId `
-  -TemplateFile $templateFile -TemplateParameterFile $paramterFile -adminUsername $userName -adminPassword $passWordSecure -Verbose
+  -TemplateFile $templateFile -TemplateParameterFile $paramterFile -adminUsername $userName -adminPassword $passWordSecure -WhatIf
 # verbose or debug or WhatIf for actually deploying it
 
-# Test success 15.01.2022 2. Windows VM in rg xx, deployed to vnet/subnet in rg yy ?
+# Test success 15.01.2022 2. Linux VM in rg xx, deployed to vnet/subnet in rg yy
