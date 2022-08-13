0c7d3b34c46b633e75de5a66a07c07ce0c6960c7
spawnmarvel <espenkleivane@gmail.com>
2022-08-13 21:53:47 +0200

    bearer token comment

--- 8_extra_tutorials/vm_managed_identiti_ps1/upload_file.ps1
+++ 8_extra_tutorials/vm_managed_identiti_ps1/upload_file.ps1
@@ -17,6 +17,7 @@ Write-Host $ArmToken
 # First, convert the parameters to JSON, then call the storage listServiceSas endpoint to create the SAS credential:
 $params = @{canonicalizedResource="/blob/testit3straccount/testmaidentity";signedResource="c";signedPermission="rcw";signedProtocol="https";signedExpiry="2022-08-14T00:00:00Z"}
 $jsonParams = $params | ConvertTo-Json
+# Bearer token is used here
 $sasResponse = Invoke-WebRequest -Uri https://management.azure.com/subscriptions/xxx-findit-under-subscriptions-or-use-ps1-to-get-it/resourceGroups/test-it3/providers/Microsoft.Storage/storageAccounts/testit3straccount/listServiceSas/?api-version=2017-06-01 -Method POST -Body $jsonParams -Headers @{Authorization="Bearer $ArmToken"}
 
 # 5 Now we can extract the SAS credential from the response:
