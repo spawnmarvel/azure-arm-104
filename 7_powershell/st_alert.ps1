f517b140b84413833e43f4e913944c6bce773ebd
Espen <espenkleivane@gmail.com>
2022-12-09 15:53:36 +0100

    st alert 3

--- 7_powershell/st_alert.ps1
+++ 7_powershell/st_alert.ps1
@@ -5,10 +5,13 @@ Write-Host $rv.TcpTestSucceeded
 # Resolves to True or False
 
 
-$stacc = "staccountname.file.core.windows.net"
+$stacc = "staaccname.file.core.windows.net"
 $rv = Test-NetConnection -ComputerName $stacc -Port 445 | select TcpTestSucceeded
 # status
-Start-Process -NoNewWindow -FilePath "C:\OP\Zabbix Agent 6 arc\zabbix_agent-6.0.0-windows-amd64-openssl\bin\zabbix_sender.exe" -ArgumentList "-z ip -p 10051 -s EWEQWAPA1PET01 -k Pingstorageaccount -o $rv"
-# name
-Start-Process -NoNewWindow -FilePath "C:\OP\Zabbix Agent 6 arc\zabbix_agent-6.0.0-windows-amd64-openssl\bin\zabbix_sender.exe" -ArgumentList "-z ip -p 10051 -s EWEQWAPA1PET01 -k Pingstorageaccountname -o $stacc"
+Start-Process -NoNewWindow -FilePath "C:\OP\Zabbix Agent 6 arc\zabbix_agent-6.0.0-windows-amd64-openssl\bin\zabbix_sender.exe" -ArgumentList "-z ip -p 10051 -s EWEQWAPA1PET01 -k Pings288weqassaa1p2 -o $rv"
+
+$da = Get-Date
+
+Add-Content "C:\OP\Zabbix Agent 6 arc\Ims_oc_logger.txt" "$da Run done"
+
 
