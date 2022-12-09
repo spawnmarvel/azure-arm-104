4c2189f797dfe3f37efd68579b16aa0a166699ad
Espen <espenkleivane@gmail.com>
2022-12-09 14:52:38 +0100

    st alert 2

--- 7_powershell/st_alert.ps1
+++ 7_powershell/st_alert.ps1
@@ -1,5 +1,14 @@
 $rv = Test-NetConnection -ComputerName staccountname.file.core.windows.net -Port 445
-# can access all ffileds from cmd
+# Can access all ffileds from cmd
 Write-Host $rv.TcpTestSucceeded
 
-Resolves to True or False
+# Resolves to True or False
+
+
+$stacc = "staccountname.file.core.windows.net"
+$rv = Test-NetConnection -ComputerName $stacc -Port 445 | select TcpTestSucceeded
+# status
+Start-Process -NoNewWindow -FilePath "C:\OP\Zabbix Agent 6 arc\zabbix_agent-6.0.0-windows-amd64-openssl\bin\zabbix_sender.exe" -ArgumentList "-z ip -p 10051 -s EWEQWAPA1PET01 -k Pingstorageaccount -o $rv"
+# name
+Start-Process -NoNewWindow -FilePath "C:\OP\Zabbix Agent 6 arc\zabbix_agent-6.0.0-windows-amd64-openssl\bin\zabbix_sender.exe" -ArgumentList "-z ip -p 10051 -s EWEQWAPA1PET01 -k Pingstorageaccountname -o $stacc"
+
